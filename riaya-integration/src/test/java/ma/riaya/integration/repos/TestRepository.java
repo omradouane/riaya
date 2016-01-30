455a4efe7ebb27e017b85163f60a69dc87a74822
radouane <om.radouane@gmail.com>
2016-01-30 11:35:25 +0000

    Add repository unit test

---
+++ riaya-integration/src/test/java/ma/riaya/integration/repos/TestRepository.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ma.riaya.integration.repos;
+
+import static org.junit.Assert.assertNotNull;
+
+import ma.riaya.integration.exception.IntegrationException;
+import ma.riaya.model.dictionary.BaseObject;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:om.radouane@gmail.com">Radouane OULEDMOUSSA</a>
+ *
+ */
+public class TestRepository {
+
+	private static  IRepository<BaseObject> repos;
+
+	@BeforeClass
+	public static void setUp() {
+		repos = new RepositoryImpl<BaseObject>(BaseObject.class);
+	}
+	
+	@Test
+	public void validateRepos() throws IntegrationException {
+		assertNotNull(repos);
+		assertNotNull(repos.getEm());
+		assertNotNull(repos.getDomainClass());
+	}
+}
