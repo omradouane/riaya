50bfafcf0c6a1a919974ac3cd750bd0c9dffc732
radouane <om.radouane@gmail.com>
2016-07-15 14:42:25 +0000

    Add service module

---
+++ riaya-service/src/main/java/ma/riaya/service/AuthenticationService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ma.riaya.service;
+
+import ma.riaya.integration.repos.IUserRepository;
+
+/**
+ * @author <a href="mailto:om.radouane@gmail.com">Radouane OULEDMOUSSA</a>
+ *
+ */
+public class AuthenticationService {
+
+	//
+	private IUserRepository userRepository;
+	
+	public void authenticate(final String login) {
+		
+	}
+}
